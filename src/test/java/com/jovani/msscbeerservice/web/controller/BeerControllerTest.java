4732868ede748d83f2d18f07e6e075551fb27e37
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2019-12-27 11:00:03 -0300

    Add controller tests. Closes #4

---
+++ src/test/java/com/jovani/msscbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,56 @@
+package com.jovani.msscbeerservice.web.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jovani.msscbeerservice.web.model.BeerDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class BeerControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void getBeer() throws Exception {
+        this.mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void saveBeer() throws Exception {
+        BeerDto beerDto = BeerDto.builder().build();
+        String beerDtoJson = this.objectMapper.writeValueAsString(beerDto);
+
+        this.mockMvc.perform(post("/api/v1/beer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void updateBeer() throws Exception {
+        BeerDto beerDto = BeerDto.builder().build();
+        String beerDtoJson = this.objectMapper.writeValueAsString(beerDto);
+
+        this.mockMvc.perform(put("/api/v1/beer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+                .andExpect(status().isNoContent());
+    }
+}
