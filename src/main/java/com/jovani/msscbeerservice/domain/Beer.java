6cf3533d2af4f48391263ec29c3a4b4f3614f319
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2019-12-27 23:56:53 -0300

    Add Beer JPA entity. Closes #6

---
+++ src/main/java/com/jovani/msscbeerservice/domain/Beer.java
@@ -0,0 +1,40 @@
+package com.jovani.msscbeerservice.domain;
+
+import com.jovani.msscbeerservice.web.model.BeerStyleEnum;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+@Entity(name = "beers")
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+    @Version // for optimistic locking
+    private Integer version;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private OffsetDateTime createdDate;
+    @UpdateTimestamp
+    private OffsetDateTime lastModifiedDate;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+    @Column(unique = true)
+    private Long upc;
+    private Integer quantityToBrew;
+
+}
