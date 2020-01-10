368fc3559944c1df2eb0f43c14638671210ccd22
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2020-01-10 17:24:34 -0300

    Add Jackson config to BeerDTO. Closes #13

--- src/main/java/com/jovani/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/jovani/msscbeerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package com.jovani.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,18 +24,20 @@ public class BeerDto {
     private UUID id;
     @Null
     private Integer version;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime createdDate;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime lastModifiedDate;
-
     @NotBlank
     private String beerName;
     @NotNull
     private BeerStyleEnum beerStyle;
-    @Positive
     @NotNull
     private Long upc;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    @Positive
     @NotNull
     private BigDecimal price;
 
