2ce5434e21a48ffe775805dfb68316bb42f9b5c8
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2020-02-05 00:27:44 -0300

    Implement validate order listener. Closes #22

---
+++ src/main/java/com/jovani/msscbeerservice/events/ValidateOrderRequest.java
@@ -0,0 +1,13 @@
+package com.jovani.msscbeerservice.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ValidateOrderResultEvent {
+}
---
+++ src/main/java/com/jovani/msscbeerservice/services/order/BeerOrderValidator.java
@@ -0,0 +1,4 @@
+package com.jovani.msscbeerservice.services.order;
+
+public class BeerOrderValidator {
+}
